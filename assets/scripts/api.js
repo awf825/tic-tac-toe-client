0e98a2d30d98c1f942af6de8f10a20ca00d75833
awf825 <faiden454@gmail.com>
2019-04-03 16:02:56 -0400

    Commit 3
    Worked DRY code for game board,
    still need to keep squares from
    being reselected

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,1 +1,1 @@
-const config = require(./config)
+
--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,13 +1,91 @@
 // const ui = require(./ui.js)
 // const api = require(./api.js)
 // const getFormFields = require(../../lib/get-form-fields.js)
-
-const test = function (event) {
-  event.preventDefault()
-
-  console.log('a square was clicked', event)
+const board = ['', '', '', '', '', '', '', '', '']
+let currentPlayer = 'X'
+const turn = function () {
+  if (currentPlayer === 'O') {
+    currentPlayer = 'X'
+  } else currentPlayer = 'O'
 }
 
+const onClick = function (event) {
+  const currentBox = $(event.target).data('id')
+  $(event.target).text(currentPlayer)
+  board[currentBox] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const addEventHandler = function () {
+  $('.square').on('click', onClick)
+}
+/* const onClick1 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[1] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick2 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[2] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick3 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[3] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick4 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[4] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick5 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[5] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick6 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[6] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick7 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[7] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}
+
+const onClick8 = function (event) {
+  $(event.target).text(currentPlayer)
+  board[8] = currentPlayer
+  turn()
+  console.log(board)
+  console.log(currentPlayer)
+}*/
 module.exports = {
-  test
+  onClick,
+  addEventHandler
+
 }
